e667b1909f65c0639de3ad86b4c5a5189f4061de
Scoin0 <john.anfernee.lewis@gmail.com>
2024-01-25 22:17:39 -1000

    Initial Commit

---
+++ Services/LoggingService.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Serilog.Events;
+using Serilog;
+
+namespace MVRB.Services
+{
+    public class LoggingService
+    {
+        public Task LogAsync(LogMessage message)
+        {
+            var severity = message.Severity switch
+            {
+                LogSeverity.Critical => LogEventLevel.Fatal,
+                LogSeverity.Error => LogEventLevel.Error,
+                LogSeverity.Warning => LogEventLevel.Warning,
+                LogSeverity.Info => LogEventLevel.Information,
+                LogSeverity.Verbose => LogEventLevel.Verbose,
+                LogSeverity.Debug => LogEventLevel.Debug,
+                _ => LogEventLevel.Information
+            };
+            Log.Write(severity, message.Exception, "[{Source}] {message}", message.Source, message.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
