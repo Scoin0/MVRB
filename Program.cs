28f9ee2e138a7f258937b833392888d42b27e5aa
John Lewis <HobitS282@yahoo.com>
2024-01-28 17:13:26 -1000

    Change structure of Program.cs class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 ﻿using MVRB.Services;
+using Serilog;
 
 public class Program
 {
-    static void Main(string[] args) => new DiscordService().InitAsync().GetAwaiter().GetResult();
+    static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        new DiscordService().InitAsync().GetAwaiter().GetResult();
+    }
 }
