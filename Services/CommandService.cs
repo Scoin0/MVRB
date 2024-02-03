c3375f23d7d47f056fd9e92f3443a9595841c76f
Scoin0 <john.anfernee.lewis@gmail.com>
2024-02-03 00:07:07 -1000

    Handle Slash Commands

---
+++ Services/CommandService.cs
@@ -0,0 +1,135 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Serilog;
+using System.Reflection;
+
+// I'm dumb. I could not understand commands. This comes from https://www.gngrninja.com/code/2019/4/1/c-discord-bot-command-handling
+// TODO: Write my own?
+
+namespace MVRB.Services
+{
+    public class CommandService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _commands;
+        private readonly IServiceProvider _services;
+
+        public CommandService(DiscordSocketClient client, InteractionService commands, IServiceProvider services)
+        {
+            _client = client;
+            _commands = commands;
+            _services = services;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+            _client.InteractionCreated += HandleInteraction;
+
+            _commands.SlashCommandExecuted += SlashCommandExecuted;
+            _commands.ContextCommandExecuted += ContextCommandExecuted;
+            _commands.ComponentCommandExecuted += ComponentCommandExecuted;
+        }
+
+        private async Task ComponentCommandExecuted(ComponentCommandInfo info, Discord.IInteractionContext context, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                switch (result.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        await context.Interaction.RespondAsync($"Unmet Precondition: {result.ErrorReason}", null, false, true);
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        await context.Interaction.RespondAsync("Unknown Command", null, false, true);
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        await context.Interaction.RespondAsync("Invalid number or arguments", null, false, true);
+                        break;
+                    case InteractionCommandError.Exception:
+                        await context.Interaction.RespondAsync($"Command Exception: {result.ErrorReason}", null, false, true);
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        await context.Interaction.RespondAsync("Command could not be executed", null, false, true);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private async Task ContextCommandExecuted(ContextCommandInfo info, Discord.IInteractionContext context, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                switch (result.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        await context.Interaction.RespondAsync($"Unmet Precondition: {result.ErrorReason}", null, false, true);
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        await context.Interaction.RespondAsync("Unknown Command", null, false, true);
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        await context.Interaction.RespondAsync("Invalid number or arguments", null, false, true);
+                        break;
+                    case InteractionCommandError.Exception:
+                        await context.Interaction.RespondAsync($"Command Exception: {result.ErrorReason}", null, false, true);
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        await context.Interaction.RespondAsync("Command could not be executed", null, false, true);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private async Task SlashCommandExecuted(SlashCommandInfo info, Discord.IInteractionContext context, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                switch (result.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        await context.Interaction.RespondAsync($"Unmet Precondition: {result.ErrorReason}", null, false, true);
+                        break;
+                    case InteractionCommandError.UnknownCommand:
+                        await context.Interaction.RespondAsync("Unknown Command", null, false, true);
+                        break;
+                    case InteractionCommandError.BadArgs:
+                        await context.Interaction.RespondAsync("Invalid number or arguments", null, false, true);
+                        break;
+                    case InteractionCommandError.Exception:
+                        await context.Interaction.RespondAsync($"Command Exception: {result.ErrorReason}", null, false, true);
+                        break;
+                    case InteractionCommandError.Unsuccessful:
+                        await context.Interaction.RespondAsync("Command could not be executed", null, false, true);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private async Task HandleInteraction(SocketInteraction arg)
+        {
+            try
+            {
+                var context = new SocketInteractionContext(_client, arg);
+                await _commands.ExecuteCommandAsync(context, _services);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+
+                if (arg.Type == InteractionType.ApplicationCommand)
+                {
+                    await arg.GetOriginalResponseAsync().ContinueWith(async (msg) => await msg.Result.DeleteAsync());
+                }
+            }
+        } 
+    }
+}
--- Services/DiscordService.cs
+++ Services/DiscordService.cs
@@ -2,6 +2,7 @@
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using MVRB.Configuration;
+using Discord.Interactions;
 
 namespace MVRB.Services
 {
@@ -10,14 +11,16 @@ namespace MVRB.Services
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private readonly DiscordSocketClient _client;
         private readonly ServiceProvider _services;
+        private readonly InteractionService _commands;
         private readonly LoggingService _loggingService;
-        public BotConfig botConfig = new BotConfig();
+        public static BotConfig botConfig = new BotConfig();
 
         public DiscordService()
         {
             _services = ConfigureServices();
             _loggingService = _services.GetRequiredService<LoggingService>();
             _client = _services.GetRequiredService<DiscordSocketClient>();
+            _commands = _services.GetRequiredService<InteractionService>();
         }
 
         public async Task InitAsync()
@@ -27,16 +30,44 @@ namespace MVRB.Services
             ValidateConfig();
 
             _client.Log += _loggingService.LogAsync;
+            _commands.Log += _loggingService.LogAsync;
+            _client.Ready += Ready;
 
             await RunBotAsync();
         }
 
         public async Task RunBotAsync()
         {
+
             await _client.LoginAsync(TokenType.Bot, botConfig.Token);
             await _client.StartAsync();
+            await _services.GetRequiredService<CommandService>().InitializeAsync();
 
             await Task.Delay(-1, _cancellationTokenSource.Token);
+        }
+
+        private void ValidateConfig()
+        {
+            if (string.IsNullOrWhiteSpace(botConfig.Token))
+            {
+                throw new Exception("Token is missing from the config! Please enter the token.");
+            }
+        }
+
+        private async Task Ready()
+        {
+            await _commands.RegisterCommandsGloballyAsync(true);
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<BotConfig>()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<CommandService>()
+                .AddSingleton<LoggingService>()
+                .BuildServiceProvider();
         }
 
         public void StopBot()
@@ -48,23 +79,6 @@ namespace MVRB.Services
         {
             _services.Dispose();
             _cancellationTokenSource.Dispose();
-        }
-
-        private void ValidateConfig()
-        {
-            if (string.IsNullOrWhiteSpace(botConfig.Token))
-            {
-                throw new Exception("Token is missing from the config! Please enter the token.");
-            }
-        }
-
-        private ServiceProvider ConfigureServices()
-        {
-            return new ServiceCollection()
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton<LoggingService>()
-                .AddSingleton<BotConfig>()
-                .BuildServiceProvider();
         }
     }
 }
